3e6057590e766f08414caa6e407b9551edbe7d95
asyy <alexys.guerin@hotmail.fr>
2021-02-17 11:34:38 +0100

    Output vers LCM OK + pleins de résulat lCM

--- src/App.java
+++ src/App.java
@@ -1,16 +1,34 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class App {
+	static String filename = "files/all_absolute+.txt";
+	//String filename = "files/test.txt";
+	//System.out.println(new File(filename).getAbsolutePath());
+
+	static HashSet<String> joueurA = new HashSet<>();
+	static HashSet<String> joueurB = new HashSet<>();
+
+	static List<HashSet<String>> total = new ArrayList<>();
+	static List<HashSet<Integer>> totalMetEnInt = new ArrayList<>();
+	static List<String> banList = Arrays.asList(
+			"armorup!","daggermastery","fireblast","lesserheal","lifetap",
+			"reinforce","shapeshift","steadyshot","totemiccall","egin","begin","thecoin");
+	static int numPartie = 0;
+
 	static Map<Integer, String> deckDictionnary = new HashMap<>();
 	static int lastUpdatedIndex = 1;
-	
+
 	public static void updateDictionnary(String item) {
 		if(!deckDictionnary.containsValue(item)){
 			deckDictionnary.put(lastUpdatedIndex, item);
@@ -29,37 +47,25 @@ public class App {
 	public static boolean isLast(List<String> l, String line) {
 		return l.get(l.size()-1).equals(line);
 	}
-	public static void main(String[] args) throws Exception {
-		String filename = "files/all_absolute+.txt";
-		//String filename = "files/test.txt";
-		System.out.println(new File(filename).getAbsolutePath());
-		List<String> l = new ArrayList<>();
-		HashSet<String> joueurA = new HashSet<>();
-		HashSet<String> joueurB = new HashSet<>();
-		
-		List<HashSet<String>> total = new ArrayList<>();
-		List<HashSet<String>> totalMetEnInt = new ArrayList<>();
-		List<String> banList = Arrays.asList(
-				"armorup!","daggermastery","fireblast","lesserheal","lifetap",
-				"reinforce","shapeshift","steadyshot","totemiccall","egin","begin","thecoin");
-		int numPartie = 0;
-		
+
+	public static void input() throws FileNotFoundException {
 		try (Scanner scanner = new Scanner(new File(filename));) {
 			scanner.useDelimiter("");
+			List<String> l = new ArrayList<>();
 
 			while(scanner.hasNext()) {
 				l.add(scanner.nextLine());
 
 			}
-			
+
 			for(String line : l) {
 
 				String[] oneLine = line.split(" ");
 				String cardName = oneLine[1].substring(1);
 				if(!banList.contains(cardName.toLowerCase())){
-											
+
 					updateDictionnary(cardName);	
-				
+
 					if(oneLine[1].charAt(0) == 'M') {
 
 						joueurA.add(cardName);
@@ -70,13 +76,13 @@ public class App {
 				}
 
 				if(numPartie != Integer.valueOf(oneLine[0]) || isLast(l,line)) {
-
+					/*
 					for(String s : joueurA) {
 						System.out.println("jA " + s);
 					}
 					for(String s : joueurB) {
 						System.out.println("jB " + s);
-					}
+					}*/
 					total.add(joueurA);
 					total.add(joueurB);
 					numPartie = Integer.valueOf(oneLine[0]);
@@ -85,58 +91,97 @@ public class App {
 				}
 
 			}
-			System.out.println("////////////////////////////");
-			System.out.println("SIZE total " + total.size());
-			System.out.println("SIZE bdd " + deckDictionnary.size());
-			String res = "";
-			int cpt = 0;
-			
-			
-			
-			
-			HashSet<String> transversation = new HashSet<>();
-			for(HashSet<String> h : total) {
+		}
+		HashSet<Integer> transversation = new HashSet<>();
+		for(HashSet<String> h : total) {
 
-				for(String s : h) {
-					String id = search(s);
-					if(!id.equals("null")) {
-						transversation.add(id);
-					}else {
-						System.out.println("GROSEZAKEZAJLKEAZ");
-					}
+			for(String s : h) {
+				String id = search(s);
+				if(!id.equals("null")) {
+					transversation.add(Integer.valueOf(id));
+				}else {
+					System.out.println("GROSEZAKEZAJLKEAZ");
 				}
-				totalMetEnInt.add(transversation);
-				transversation = new HashSet<>();
 			}
-			for(HashSet<String> h : total) {
-				//System.out.println(h.size());
-				for(String s : h) {
-
-					//System.out.println(cpt +" " + s);
-
-				}
-				cpt++;
-			}
-			cpt = 0;
-			for(HashSet<String> h : totalMetEnInt) {
-				//System.out.println(h.size());
-				for(String s : h) {
-
-					//System.out.println(cpt +" " + s);
-
-				}
-				cpt++;
-			}
-			/*int cpt2 = 0;
-			for(String s : bddCarte) {
-				System.out.println(cpt2 +" " + s);
-				cpt2++;
-			}*/
-			
-			for(Integer truc : deckDictionnary.keySet()) {
-				System.out.println(truc +" "+deckDictionnary.get(truc));
-			}
+			totalMetEnInt.add(transversation);
+			transversation = new HashSet<>();
 		}
 	}
+	/*
+	public static void afficheDeckEnInt() {
+		int cpt = 0;
+		for(HashSet<Integer> h : totalMetEnInt) {
+			System.out.println(h.size());
+			for(String s : h) {
+				System.out.println(cpt +" " + s);
+			}
+			cpt++;
+		}
+	}*/
+
+	public static void afficheDeckName() {
+		int cpt = 0;
+		for(HashSet<String> h : total) {
+			System.out.println("Size deck : "+h.size());
+			for(String s : h) {
+				System.out.println(cpt +" " + s);
+			}
+			cpt++;
+		}
+	}
+
+	public static void afficheBDD() {
+		for(Integer truc : deckDictionnary.keySet()) {
+			System.out.println(truc +" "+deckDictionnary.get(truc));
+		}
+	}
+	public static String DeckList(){
+		
+		String res = "";
+		List<Integer> hSorted = new ArrayList<>();
+		for(HashSet<Integer> h : totalMetEnInt) {
+			hSorted = new ArrayList<>(h);
+			Collections.sort(hSorted);
+			
+			for(Integer s : hSorted){
+				res += s.toString()+" ";
+			}
+			res+="\n";
+		}
+		return res;
+	}
 	
+
+	public static String joliId() {
+		String res ="@CONVERTED_FROM_TEXT \n";
+		for(Integer key : deckDictionnary.keySet()) {
+			res += "@ITEM="+key+"="+deckDictionnary.get(key)+"\n";
+		}
+		return res;
+	}
+	public static void output() {
+		try {
+			FileWriter output = new FileWriter("./files/output1.txt");
+			output.write(joliId());
+			output.write(DeckList());
+			output.close();
+			
+		}catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+
+	public static void main(String[] args) throws Exception {
+		input();
+		//afficheDeckEnInt();
+		//afficheDeckName();
+		//afficheBDD();
+		//System.out.println(DeckList());
+		output();
+
+	}
 }
+
+
